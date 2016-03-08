4b25673a99273a934e6cbd3bbcf3f55da142f801
Manuel Galilea <manuel.galilea@gmail.com>
2016-03-08 02:33:34 +0100

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Calculate the inverse of a matrix and storage in the cache m_inv variable
+## Also storage the original matrix to compare
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        if (is.matrix(x)){ 
+                m <<- x
+                m_inv <<- solve(x)
+        } else {
+                print("Data is not a matrix") 
+        }
 }
 
 
@@ -12,4 +18,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if (is.matrix(x)){
+                if (identical(x,m)){
+                        ## When matrix are identical use the cache matrix
+                        m_inv
+                } else {
+                        ## When matrix are not equal, calculate the inverse
+                        solve(x)
+                }
+                       
+        } else {
+                print("Data is not a matrix")
+        }
 }
